1e7ade1973647407313f9446c3995f5d30ca92e1
Templar-101 <hcokorie@gmail.com>
2026-02-10 06:45:26 +0000

    Remove assume_role block

--- provider.tf
+++ provider.tf
@@ -11,9 +11,5 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-
-  assume_role {
-    role_arn     = "arn:aws:iam::886036506684:role/HencoProject1OIDCRole"
-    session_name = "github-actions"
-  }
 }
+
